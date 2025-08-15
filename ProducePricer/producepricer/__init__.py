279682f47a830b460f8fc87eed5c38e2eccf472d
Cade Stocker <caderstocker@gmail.com>
2025-08-15 13:07:39 -0400

    turned off debug

--- ProducePricer/producepricer/__init__.py
+++ ProducePricer/producepricer/__init__.py
@@ -29,7 +29,7 @@ def create_app(db_uri=None):
     app = Flask(__name__)
 
     # turn back off
-    app.config['DEBUG'] = True
+    #app.config['DEBUG'] = True
 
     # --- START: Production Database Configuration ---
     # This logic checks if it's running on Render by looking for the mounted disk.
